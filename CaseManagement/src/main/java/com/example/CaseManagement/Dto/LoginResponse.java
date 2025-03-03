32dd2feb474e213362670208a4768c8de720d640
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-03-03 02:03:01 -0800

    resolved getters and setters

--- CaseManagement/src/main/java/com/example/CaseManagement/Dto/LoginResponse.java
+++ CaseManagement/src/main/java/com/example/CaseManagement/Dto/LoginResponse.java
@@ -30,47 +30,4 @@ public class LoginResponse {
         this.role = user.getRole().toString();
     }
 
-    //getters and setters
-    public String getUserId(){
-        return userId;
-    }
-    public void setUserId(String userId){
-        this.userId = userId;
-    }
-
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public String getPhone(){
-        return phone;
-    }
-    public void setPhone(String phone){
-        this.phone = phone;
-    }
-
-    public String getProfilePictureUrl(){
-        return profilePictureUrl;
-    }
-    public void setProfilePictureUrl(String profilePictureUrl){
-        this.profilePictureUrl = profilePictureUrl;
-    }
-
-    public String getRole(){
-        return role;
-    }
-    public void setRole(String role){
-        this.role = role;
-    }
-
 }
