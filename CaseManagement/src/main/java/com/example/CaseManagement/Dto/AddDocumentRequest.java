7066d05dab8f8cdfbc305218c7c70cf6657edc32
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-03-07 04:23:12 -0800

    Add document request

---
+++ CaseManagement/src/main/java/com/example/CaseManagement/Dto/AddDocumentRequest.java
@@ -0,0 +1,16 @@
+package com.example.CaseManagement.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class AddDocumentRequest {
+    private Long caseId;
+    private String fileName;
+    private String fileUrl;
+}
