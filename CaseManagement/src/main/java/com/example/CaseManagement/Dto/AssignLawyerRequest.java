eccb50da93ac9dac0aa6116fb99bb0ce97ae314a
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-03-07 04:20:49 -0800

    assign Lawyer request dto

--- CaseManagement/src/main/java/com/example/CaseManagement/Dto/AssignLawyerRequest.java
+++ CaseManagement/src/main/java/com/example/CaseManagement/Dto/AssignLawyerRequest.java
@@ -1,4 +1,15 @@
 package com.example.CaseManagement.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class AssignLawyerRequest {
+    private Long caseId;
+    private Long lawyerId;
 }
