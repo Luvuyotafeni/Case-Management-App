6cac416a49b6be95d06c4056e39dbdd0acbc6e2e
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-03-07 04:35:27 -0800

    Case Response

---
+++ CaseManagement/src/main/java/com/example/CaseManagement/Dto/CaseResponse.java
@@ -0,0 +1,22 @@
+package com.example.CaseManagement.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class CaseResponse {
+    private Long caseId;
+    private String caseName;
+    private String caseNumber;
+    private String caseType;
+    private String description;
+    private String stationName;
+    private String province;
+    private String officerName;
+    private String officerContact;
+}
