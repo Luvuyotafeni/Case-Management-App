51027077e078a0a66368e1ad6650cb464a2491b4
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-03-17 00:54:05 -0700

    passowrd Reset service

--- CaseManagement/src/main/java/com/example/CaseManagement/repository/CredentialsRepository.java
+++ CaseManagement/src/main/java/com/example/CaseManagement/repository/CredentialsRepository.java
@@ -4,8 +4,10 @@ import com.example.CaseManagement.entity.CredentialsEntity;
 import com.example.CaseManagement.entity.UserBaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CredentialsRepository extends JpaRepository<CredentialsEntity, Long> {
     CredentialsEntity findByUser(UserBaseEntity user);
+    Optional<CredentialsEntity> findByUserId(Long userId);
 }
---
+++ CaseManagement/src/main/java/com/example/CaseManagement/service/impl/PasswordResetService.java
@@ -0,0 +1,104 @@
+package com.example.CaseManagement.service.impl;
+
+
+import com.example.CaseManagement.entity.PasswordResetTokenEntity;
+import com.example.CaseManagement.entity.UserBaseEntity;
+import com.example.CaseManagement.repository.CredentialsRepository;
+import com.example.CaseManagement.repository.PasswordResetTokenRepository;
+import com.example.CaseManagement.service.UserService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class PasswordResetService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordResetTokenRepository tokenRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CredentialsRepository credentialsRepository;
+
+    //Generating a random 6-digit otp
+    private String generateOtp(){
+        Random random = new Random();
+        int otp = 100000 + random.nextInt(900000);
+        return String.valueOf(otp);
+    }
+
+
+    @Transactional
+    public void requestPasswordReset(String email){
+        try {
+            //checking if user exists
+            UserBaseEntity user = userService.getUserByEmail(email);
+
+            String otp = generateOtp();
+
+            PasswordResetTokenEntity token = new PasswordResetTokenEntity();
+            token.setEmail(email);
+            token.setOtp(otp);
+            token.setExpiryDate(LocalDateTime.now().plusMinutes(10));
+            tokenRepository.save(token);
+
+            emailService.sendResetOtpEmail(email, otp);
+        } catch (RuntimeException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Transactional
+    public boolean validateOtp(String email, String otp){
+        Optional<PasswordResetTokenEntity> tokenOtp = tokenRepository.findByEmailAndOtpAndUsedFalse(email, otp);
+
+        if (tokenOtp.isPresent()){
+            PasswordResetTokenEntity token = tokenOtp.get();
+
+            if (token.isExpired()){
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Transactional
+    public void resetPassword(String email, String otp, String newPassword){
+        Optional<PasswordResetTokenEntity> tokenOtp = tokenRepository.findByEmailAndOtpAndUsedFalse(email, otp);
+
+        if (tokenOtp.isEmpty() || tokenOtp.get().isExpired()){
+            throw new RuntimeException("Invalid or expired otp");
+        }
+
+        PasswordResetTokenEntity token = tokenOtp.get();
+        UserBaseEntity user = userService.getUserByEmail(email);
+
+
+        var credentials = credentialsRepository.findByUserId(user.getUserId())
+                .orElseThrow(()-> new RuntimeException("credentials not found"));
+
+        credentials.setPassword(passwordEncoder.encode(newPassword));
+        credentialsRepository.save(credentials);
+
+
+        token.setUsed(true);
+        tokenRepository.save(token);
+
+    }
+}
