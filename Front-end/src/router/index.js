a0bbc5cbafbc98413b6e762002aaf91b9dc2abd4
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-03-19 02:11:37 -0700

    header component

--- Front-end/src/router/index.js
+++ Front-end/src/router/index.js
@@ -3,7 +3,14 @@ import ForgotPassword from "@/components/ForgotPassword.vue";
 import LoginPage from "@/components/LoginPage.vue";
 import Register from "@/components/Register.vue";
 import Welcome from "@/components/Welcome.vue";
+import AdminProfile from "@/components/AdminComponents/Profile.vue";
+import AdminDashboard from "@/components/AdminComponents/AdminDashboard.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import Cases from "@/components/AdminComponents/Cases.vue";
+import Documents from "@/components/AdminComponents/Documents.vue";
+import AdminUsers from "@/components/AdminComponents/AdminUsers.vue";
+import Lawyers from "@/components/AdminComponents/Lawyers.vue";
+import Admins from "@/components/AdminComponents/Admins.vue";
 
 const routes = [
     {
@@ -31,6 +38,72 @@ const routes = [
         name: "dashboard",
         component: Dashboard
     }
+    ,
+    {
+        path: "/admin-profile",
+        name: "admin-profile",
+        component: AdminProfile
+    }
+    ,
+    {
+        path: "/admin-dashboard",
+        name: "admin-dashboard",
+        component: AdminDashboard
+    }
+    ,
+    {
+        path: "/admin-cases",
+        name: "admin-cases",
+        component: Cases
+    }
+    ,
+    {
+        path: "/admin-documents",
+        name: "admin-documents",
+        component: Documents
+    }
+    ,
+    {
+        path: "/admin-users",
+        name: "admin-users",
+        component: AdminUsers
+    },
+    {
+        path: "/lawyers",
+        name: "lawyers",
+        component: Lawyers
+    },
+    {
+        path: "/admins",
+        name: "admins",
+        component: Admins
+    },
+    {
+        path: "/admin-users",
+        name: "admin-users",
+        component: AdminUsers
+    },
+    {
+        path: "/admin-users",
+        name: "admin-users",
+        component: AdminUsers
+    },
+    {
+        path: "/admin-users",
+        name: "admin-users",
+        component: AdminUsers
+    },
+    {
+        path: "/admin-users",
+        name: "admin-users",
+        component: AdminUsers
+    },
+    {
+        path: "/admin-users",
+        name: "admin-users",
+        component: AdminUsers
+    }
+
 ];
 
 const router = createRouter({
