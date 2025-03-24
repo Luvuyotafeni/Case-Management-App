5ef9d95d1e77840fed442127f2f9b9b76907a909
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-03-24 06:55:06 -0700

    successfully shows the lawyers assigned cases

--- Front-end/src/components/LawyerComponents/Cases.vue
+++ Front-end/src/components/LawyerComponents/Cases.vue
@@ -1,14 +1,55 @@
 <script setup>
- import { ref } from 'vue';
-import Header from './Header.vue';
+ import { onMounted, ref } from 'vue';
+  import Header from './Header.vue';
+import CaseService from '@/services/CaseService';
  const cases = ref([]);
+
+ const userId = sessionStorage.getItem("userId");
+
+ const fetchAssignedCases = async ()=>{
+  try{
+    const response = await CaseService.getCaseByAssignedLaywyerUserId(userId);
+    cases.value = response;
+  } catch(error){
+    console.log("Error fetching the cases for the user: ", error);
+  }
+ };
+
+ onMounted(fetchAssignedCases);
 </script>
 <template>
      <div class="container">
         <Header/>
         <div class="dashboard">
-            <p> Hello world</p>
-        </div>
+      <div class="header">
+        <h2>Cases</h2>
+        <button class="add-case">Add Case</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Case Name</th>
+            <th>Case Type</th>
+            <th>Station Name</th>
+            <th>Province</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="caseItem in cases" :key="caseItem.caseId">
+            <td>{{ caseItem.user.name }}</td>
+            <td>{{ caseItem.caseName }}</td>
+            <td>{{ caseItem.caseType }}</td>
+            <td>{{ caseItem.stationName }}</td>
+            <td>{{ caseItem.province }}</td>
+            <td>
+              <button class="see-button" @click="openModal(caseItem.caseId)">See</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     </div>
 </template>
 <style scoped>
@@ -21,11 +62,43 @@ import Header from './Header.vue';
     background-color: #F9FAFB;
   }
 
+  .header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  .add-case{
+    padding: 10px 15px;
+    background-color: #ddd;
+    color: black;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+
   .dashboard{
     display: flex;
-    flex-direction: row;
-    margin-left: 100px;
+    flex-direction: column;
+    margin-left: 20px;
     margin-top: 100px;
     width: 100%;
+  }
+
+  table{
+    width: 100%;
+    border-collapse: collapse;
+    background: white;
+  }
+
+  thead{
+    background: #ddd;
+  }
+  td, th{
+    padding: 10px;
+    border: 1px solid #ccc;
+    text-align: left;
   }
 </style>
