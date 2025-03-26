c7a10ee45f30d58c896c7950b11a7f860090be30
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-03-26 01:06:54 -0700

    create and getting lawyer admin Services

--- Front-end/src/services/AdminService.js
+++ Front-end/src/services/AdminService.js
@@ -39,6 +39,24 @@ class AdminServices {
             throw err;
         }
     }
+
+    static async createLawyer(lawyerData){
+        try{
+            const response = await axios.post(`${AdminServices.BASE_URL}api/lawyers/create`,lawyerData, this.getAuthHeaders());
+            return response.data;
+        } catch (err){
+            throw err;
+        }
+    }
+
+    static async getLawyerById(lawyerId){
+        try{
+            const response = await axios.get(`${AdminServices.BASE_URL}api/lawyers/${lawyerId}`, this.getAuthHeaders());
+            return response.data;
+        } catch(err){
+            throw err
+        }
+    }
 }
 
 export default AdminServices;
