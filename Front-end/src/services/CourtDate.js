a6f82947d6e712e72d2ee97011344772ac225d09
Luvuyotafeni <luvuyotafeni012@gmail.com>
2025-04-06 23:21:49 -0700

    authentication headers

--- Front-end/src/services/CourtDate.js
+++ Front-end/src/services/CourtDate.js
@@ -2,6 +2,16 @@ import axios from "axios";
 
 class CourtDateService {
 
+    static BASE_URL ="http://localhost:8080/";
+
+    static getAuthHeaders(){
+        const token = sessionStorage.getItem("token");
+        return {
+            headers:{
+                Authorization: `Bearer ${token}`,
+            },
+        };
+    }
 }
 
 export default CourtDateService;
--- Front-end/src/services/CourtDateNote.js
+++ Front-end/src/services/CourtDateNote.js
@@ -2,6 +2,17 @@ import axios from "axios";
 
 class CourtDateNoteService {
 
+    static BASE_URL ="http://localhost:8080/";
+
+    static getAuthHeaders(){
+        const token = sessionStorage.getItem("token");
+        return {
+            headers:{
+                Authorization: `Bearer ${token}`,
+            },
+        };
+    }
+    
 }
 
 export default CourtDateNoteService;
